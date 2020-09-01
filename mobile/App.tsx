c10876c44775cd53344243a1a63012a8c13027e1
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-01 19:48:45 -0300

    atualizando projeto mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
