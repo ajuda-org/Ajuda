4c466c4d65d9f4ca8b83119cd7c1a0ac9f8b7d15
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-03 19:23:41 -0300

    migration items

--- backend/src/database/migrations/1599092579967-createItems.ts
+++ backend/src/database/migrations/1599092579967-createItems.ts
@@ -1,38 +1,9 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createItems1599092579967 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.createTable(
-      new Table({
-        name: "items",
-        columns: [
-          {
-            name: "id",
-            type: "int",
-            isPrimary: true
-          },
-          {
-            name: "name",
-            type: "varchar",
-            isNullable: false
-          },
-          {
-            name: "image",
-            type: "varchar",
-            isNullable: false
-          },
-          {  
-            name: "created_at",
-            type: "timestamp",
-            default: "now()"
-          },
-          {
-            name: "updated_at",
-            type: "timestamp",
-            default: "now()"
-          }
-        ]
-      })
+    await queryRunner.query(
+      `CREATE TABLE items ( id SERIAL PRIMARY KEY NOT NULL, name varchar(255) NOT NULL, image varchar(255) NOT NULL, created_at TIMESTAMP DEFAULT now(), updated_at TIMESTAMP DEFAULT now() )`,
     );
   };
   
