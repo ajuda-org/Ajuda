f387a74977ad277ec308464b1e99d32a7d556fa7
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-02 21:21:03 -0300

    adição de primeira migration

---
+++ backend/src/database/migrations/1599090504385-createUsers.ts
@@ -0,0 +1,62 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class createUsers1599090504385 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isNullable: false
+          },
+          {
+            name: "cpf",
+            type: "varchar",
+            isNullable: false
+          },
+          {
+            name: "whatsapp",
+            type: "varchar",
+            isNullable: false
+          },
+          {
+            name: "type",
+            type: "varchar",
+            isNullable: false
+          },
+          {
+            name: "email",
+            type: "varchar",
+            isNullable: false,
+            isUnique: true
+          },
+          {
+            name: "password",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  };
+}
