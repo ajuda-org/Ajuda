80750c60db3cff6ef9a3ad5ffa6f8c142f0713b2
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-02 21:53:29 -0300

    criação do model de usuário

---
+++ backend/src/app/models/User.ts
@@ -0,0 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from "typeorm";
+
+@Entity("users")
+class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column("varchar")
+  name: string;
+
+  @Column("varchar")
+  cpf: string;
+
+  @Column("varchar")
+  whatsapp: string;
+
+  @Column("varchar")
+  type: string;
+
+  @Column("varchar")
+  email: string;
+
+  @Column("varchar")
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
