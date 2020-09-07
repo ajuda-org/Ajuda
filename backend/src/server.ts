1ec54373f305862c4f6d8bdc11e5445023393fb8
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-09-07 19:28:26 -0300

    adição de cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import "./database";
 import express from "express";
+import cors from "cors";
 import path from "path";
 
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
